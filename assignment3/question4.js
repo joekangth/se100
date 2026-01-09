d3c6f50477bfa5366208232b9c68ab4df16a8843
joekangth <whitelistuseonly@gmail.com>
2026-01-10 02:19:47 +0800

    Assignment 3, Question 4

---
+++ assignment3/question4.js
@@ -0,0 +1,12 @@
+const disneyData = {
+  open: "120.54",
+  high: "122.56",
+  low: "120.54",
+  close: "121.09"
+};
+
+// Add the missing key-value pair
+disneyData.volume = "1512311";
+
+// Print the updated object
+console.log(disneyData);
